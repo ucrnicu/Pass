4afd7a282090424a1551e70d73bdf15050789b9a
Nicu <Nicu@DESKTOP-7UBQ1P1>
2021-07-21 23:21:04 +0300

    Add project files.

---
+++ Pass/Controllers/PasswordController.cs
@@ -0,0 +1,30 @@
+﻿using Pass.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace Pass.Controllers
+{
+    public class PasswordController : ApiController
+    {
+        List<Password> passwords = new List<Password>();
+        public IEnumerable<Password> GetAllPasswords()
+        {
+            return passwords;
+        }
+        public IHttpActionResult PostPassword(String idClient)
+        {
+           passwords.Add(new Password(idClient, DateTime.Now));
+            if (passwords == null)
+            {
+                return NotFound();
+            }
+            return Ok(passwords);
+
+        }
+    }
+}
---
+++ Pass/Models/Password.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pass.Models
+{
+    public class Password
+    {
+        String idClient;
+        String code;
+        DateTime start;
+        DateTime end;
+
+        public DateTime End { get => end; set => end = value; }
+        public DateTime Start { get => start; set => start = value; }
+        public string Code { get => code; set => code = value; }
+        public string IdClient { get => idClient; set => idClient = value; }
+
+        public Password(String userId, DateTime timer)
+        {
+            this.idClient = userId;
+            this.start = timer;
+            this.end = timer.AddSeconds(30);
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[8];
+            var random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            this.code = new String(stringChars);
+
+
+        }
+
+    }
+}
